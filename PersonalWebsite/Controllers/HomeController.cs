e83822ae1333e6a7cd719deff1fb447d869461b8
SealabJaster <sealabjaster@gmail.com>
2019-10-26 18:05:34 +0100

    Delete code from the old version of the website.

--- PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -19,32 +19,5 @@ namespace PersonalWebsite.Controllers
         {
             return View();
         }
-
-        public IActionResult Project([FromQuery] string view)
-        {
-            return View(view);
-        }
-
-        public IActionResult Projects([FromQuery] string name, [FromQuery] string tags)
-        {
-            return View(new ProjectViewModel
-            {
-                Content = new ProjectContentViewModel { Projects = this.GetFilteredProjects(name, tags) },
-                AllTags = Data.Projects.Store.SelectMany(p => p.Tags).Distinct()
-            });
-        }
-
-        [HttpPost]
-        public IActionResult AjaxGetFilteredProjectsHtml([FromBody] ProjectFilterViewModel model)
-        {
-            return PartialView("_ProjectContentPartial", new ProjectContentViewModel{ Projects = this.GetFilteredProjects(model.Name, model.Tags) });
-        }
-
-        private IEnumerable<Project> GetFilteredProjects(string name, string tags)
-        {
-            return Data.Projects.Store
-                                .Where(p => (String.IsNullOrWhiteSpace(name)) ? true : Regex.IsMatch(p.Name, name, RegexOptions.IgnoreCase))
-                                .Where(p => (String.IsNullOrWhiteSpace(tags)) ? true : tags.Split(',').All(t => p.Tags.Select(t2 => t2.ToLower()).Contains(t.ToLower())));
-        }
     }
 }
