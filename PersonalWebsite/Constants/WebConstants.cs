d64fd03a309c7d57f5fcae0553b6ff5873004518
SealabJaster <sealabjaster@gmail.com>
2020-07-29 01:46:24 +0100

    Add WebConstants. Add a webconstant for the default og image, which is my name

---
+++ PersonalWebsite/Constants/WebConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Constants
+{
+    public static class WebConstants
+    {
+        public const string DEFAULT_OG_IMAGE = "img/default_og_image.jpg";
+    }
+}
